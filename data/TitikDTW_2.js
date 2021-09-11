e61230ef3ee8b12af06daa55696e1a558dcf2188
Rissa Rambe <71384953+rissarambe@users.noreply.github.com>
2021-09-11 18:10:19 +0700

    Add files via upload

---
+++ data/TitikDTW_2.js
@@ -0,0 +1,13 @@
+var json_TitikDTW_2 = {
+"type": "FeatureCollection",
+"name": "TitikDTW_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Bubur Ayam dan Nasi Kuning Homann, Jalan Homan, Cikawao, Bandung City, West Java", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": "-1", "extrude": "0", "visibility": "-1", "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.609927, -6.9217672 ] } },
+{ "type": "Feature", "properties": { "Name": "Museum Asia Afrika, Jalan Asia Afrika, Braga, Bandung City, West Java", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": "-1", "extrude": "0", "visibility": "-1", "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.6088366, -6.9213415 ] } },
+{ "type": "Feature", "properties": { "Name": "Mie Rica Kejaksaan, Jalan Kejaksaan, Braga, Bandung City, West Java", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": "-1", "extrude": "0", "visibility": "-1", "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.6101219, -6.9184267 ] } },
+{ "type": "Feature", "properties": { "Name": "Pasar Antik Cikapundung, Jalan ABC, Braga, Bandung City, West Java", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": "-1", "extrude": "0", "visibility": "-1", "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.6084774, -6.9189079 ] } },
+{ "type": "Feature", "properties": { "Name": "Taman Balai Kota Bandung, Jalan Wastukencana, Babakanciamis, Bandung City, West Java", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": "-1", "extrude": "0", "visibility": "-1", "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.6096114, -6.9129447 ] } },
+{ "type": "Feature", "properties": { "Name": "Kopi Toko Djawa (Braga), Jalan Braga, Braga, Bandung City, West Java", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": "-1", "extrude": "0", "visibility": "-1", "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 107.6092209, -6.9175066 ] } }
+]
+}
